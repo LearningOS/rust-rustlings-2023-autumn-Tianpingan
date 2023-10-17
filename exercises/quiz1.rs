8ae6e9976765aa1a14236c4d07a4a8c1cf393028
tianpingan <tianpingan2000@gmail.com>
2023-10-17 07:50:34 +0000

    wip: 25

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -12,11 +12,16 @@
 // quantity bought. No hints this time!
 //
 // No hints this time ;)
-
-// I AM NOT DONE
+ 
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(sum: u32) -> u32 {
+    if (sum > 40) {
+        sum 
+    } else {
+        sum * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
